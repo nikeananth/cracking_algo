543ef577546aae4cfe02dc87056511f91e064705
nikeananth <nikeananth@gmail.com>
2023-11-04 12:41:38 -0400

    binary search ceiling and floor

---
+++ src/com/kun/search/FloorOfNumber.java
@@ -0,0 +1,25 @@
+package com.kun.search;
+
+public class FloorOfNumber {
+    public static void main(String[] args) {
+         int arr[]={2,4,5,6,7,8,9,11,12};
+        int target=1;
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end) {
+            int mid=start+(end-start/2);
+            if (target == arr[mid]) {
+                System.out.println("ceil" + target + "" + mid);
+                break;
+            }
+            if(target>arr[mid]){
+                start=mid+1;
+            }else  if(target<arr[mid]){
+                end=mid-1;
+            }
+        }
+        if(end<0) {
+            System.out.println("floor does not exsit"+end);
+        }
+    }
+}
